151c58266280904ebba31bff638dbcb850309f7b
Nikita Lobaev <lobaevni@yandex-team.ru>
2021-10-01 14:15:36 +0300

    Fix main.cpp compilation error

--- main.cpp
+++ main.cpp
@@ -1,29 +1,29 @@
 #include <Eigen/Dense>
-#include <iostream>
 #include "Gauss.cpp"
-//#include "Matrix.cpp"
 
 int main() {
-    const Lobaev::Math::Matrix<double> matrix({
-        {0, 1, 3},
-        {2, 4, 6}
+    const Lobaev::Math::Matrix<double> matrix_a({
+        {0, 1},
+        {2, 4}
+    });
+    const Lobaev::Math::Vector<double> vector_f({
+        3,
+        6
     });
 
-    Lobaev::Math::Vector<double> solution(1); //stub
-    try {
-        solution = Lobaev::Math::Gauss::gauss(matrix);
-    } catch (const char *exception) {
-        std::cerr << exception << std::endl;
-        return 1;
-    }
+    const Lobaev::Math::Vector<double> solution = Lobaev::Math::Gauss::gauss(matrix_a, vector_f);
 
-    Eigen::Matrix<int, 2, 3> eigen_matrix;
-    eigen_matrix(0, 0) = 0;
-    eigen_matrix(0, 1) = 1;
-    eigen_matrix(0, 2) = 3;
-    eigen_matrix(1, 0) = 2;
-    eigen_matrix(1, 1) = 4;
-    eigen_matrix(1, 2) = 6;
+    Eigen::Matrix<int, 2, 2> eigen_matrix_a;
+    eigen_matrix_a(0, 0) = 0;
+    eigen_matrix_a(0, 1) = 1;
+    eigen_matrix_a(1, 0) = 2;
+    eigen_matrix_a(1, 1) = 4;
+
+    Eigen::Matrix<int, 2, 1> eigen_vector_f;
+    eigen_vector_f(0, 0) = 3;
+    eigen_vector_f(1, 0) = 6;
+
+    const Eigen::Matrix<int, 2, 1> eigen_vector_x = eigen_matrix_a * eigen_vector_f;
 
     return 0;
 }
